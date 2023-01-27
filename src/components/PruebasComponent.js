c624fc177b0c14962be87c13d0fd2fdee71223c7
Angel Díaz <angelmx2010@gmail.com>
2023-01-27 10:05:31 -0600

    Final del Video 45

---
+++ src/components/PruebasComponent.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react'
+
+export const PruebasComponent = () => {
+
+    const [usuario, setUsuario] = useState("Angel Díaz");
+    const [fecha, setfecha] = useState("01-01-1985");
+    const [contador, setContador] = useState(0);
+
+    const modUsuario = e => {
+        setUsuario(e.target.value);
+    };
+
+    const cambiarFecha = e => {
+        setfecha(Date.now());
+    };
+
+    // Solo al carga el componente
+    useEffect( () => {
+        console.log("Has cargado el componente PruebasComponent con useEffect");
+    }, []);
+
+    // Cuando se cambia el usuario
+    useEffect( () => {
+        setContador(contador+1);
+        console.log("Has modificado el usuario: " + contador);
+    }, [usuario]);
+
+  return (
+    <div>
+        <h1>El Efecto - Hook useEffect</h1>
+
+        <strong className={ contador >= 10 ? 'label label-green' : 'label'}>{usuario}</strong>
+
+        <strong>{fecha}</strong>
+
+        <p>
+
+            <input
+                type="text"
+                onChange={ modUsuario }
+                placeholder=" Cambiar el nombre. "
+            />
+
+            <button
+                onClick={ cambiarFecha }>
+                    Cambiar Fecha
+            </button>
+
+        </p>
+    </div>
+  )
+}
